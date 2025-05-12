6b0508a8ecc17c79146c202b32b39f6bb425b0f7
Arjun K B <arjunlal.kb@trellix.com>
2025-05-12 13:21:11 +0530

    Initial Push

---
+++ .vscode/sample_programs/checkPrime.py
@@ -0,0 +1,13 @@
+def isprime(num1):
+    if num1 <= 1:
+        return False
+    for i in range(2, num1):
+        if num1 % i == 0:
+            return False
+    return True
+
+
+print(isprime(1))
+print(isprime(2))
+print(isprime(4))
+print(isprime(11))
